d0d8c41918030470b31c0db1843efdaa8e139679
KoroPutra <putramei05@gmail.com>
2022-10-07 01:24:59 +0700

    Belajar React JS

---
+++ src/components/Mahasiswa.js
@@ -0,0 +1,17 @@
+const Mahasiswa = [
+{
+    id:"1",
+    Name:"Putra Muhamamd Ferdiansyah",
+    Age:"22"
+},{
+    id:"2",
+    Name:"Muhammad Syuqron",
+    Age:"18"
+},{
+    id:"3",
+    Name:"Putri Ayu ",
+    Age:"20"
+}
+]
+
+export default Mahasiswa;
